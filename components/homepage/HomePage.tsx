51085b72c2a9449f9337ea0cf50072b5eb2d3f08
DESKTOP-RTLN3BA\$punk <vermarohanfinal@gmail.com>
2024-08-12 18:20:40 -0700

    fix: screen button fix

--- components/homepage/HomePage.tsx
+++ components/homepage/HomePage.tsx
@@ -22,21 +22,6 @@ export function HomePage() {
                 }}
                 className="relative flex flex-col gap-4 items-center justify-center px-4"
             >
-                <Link href={'https://github.com/MODSetter/SurfSense'}>
-                    <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
-                        <span className="absolute inset-0 overflow-hidden rounded-full">
-                            <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
-                        </span>
-                        <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
-                            <span>
-                                Check Our GitHub
-                            </span>
-                            <Github />
-                        </div>
-                        <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
-                    </button>
-                </Link>
-
                 <div className="flex items-center mb-4 text-5xl font-semibold text-gray-900 dark:text-white">
                     <Image className="w-64 h-64 rounded-full" src={icon} alt="logo" />
                 </div>
@@ -53,6 +38,21 @@ export function HomePage() {
                         Sign Up
                     </Link>
                 </button>
+                <Link href={'https://github.com/MODSetter/SurfSense'}>
+                    <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+                        <span className="absolute inset-0 overflow-hidden rounded-full">
+                            <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+                        </span>
+                        <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+                            <span>
+                                Check Our GitHub
+                            </span>
+                            <Github />
+                        </div>
+                        <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+                    </button>
+                </Link>
+
             </motion.div>
         </AuroraBackground>
     );
