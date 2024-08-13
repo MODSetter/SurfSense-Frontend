32fc6af13f1776925b04af484fbbca945b8a1180
DESKTOP-RTLN3BA\$punk <vermarohanfinal@gmail.com>
2024-08-12 18:07:54 -0700

    Contacts Updates

--- components/homepage/HomePage.tsx
+++ components/homepage/HomePage.tsx
@@ -7,6 +7,7 @@ import { AuroraBackground } from "../ui/aurora-background";
 import icon from "../../public/SurfSense.png"
 import Image from "next/image";
 import Link from "next/link";
+import { Github } from "lucide-react";
 
 export function HomePage() {
     return (
@@ -21,11 +22,26 @@ export function HomePage() {
                 }}
                 className="relative flex flex-col gap-4 items-center justify-center px-4"
             >
+                <Link href={'https://github.com/MODSetter/SurfSense'}>
+                    <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+                        <span className="absolute inset-0 overflow-hidden rounded-full">
+                            <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+                        </span>
+                        <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+                            <span>
+                                Check Our GitHub
+                            </span>
+                            <Github />
+                        </div>
+                        <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+                    </button>
+                </Link>
+
                 <div className="flex items-center mb-4 text-5xl font-semibold text-gray-900 dark:text-white">
                     <Image className="w-64 h-64 rounded-full" src={icon} alt="logo" />
                 </div>
                 <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
-                    SurfSense 
+                    SurfSense
                 </div>
                 {/* <div className="text-lg font-semibold dark:text-neutral-200">Beta v0.0.1</div> */}
                 <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
