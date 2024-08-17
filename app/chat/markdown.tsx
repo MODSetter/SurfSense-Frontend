13ac97316f20ff4fd43d615667a4f52fb2e44e4e
DESKTOP-RTLN3BA\$punk <vermarohanfinal@gmail.com>
2024-08-17 03:51:58 -0700

    chore: theming to markdown

--- app/chat/markdown.tsx
+++ app/chat/markdown.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import MarkdownPreview from '@uiw/react-markdown-preview';
+import { useTheme } from 'next-themes'
 
 export default function MarkDownTest({source} : {source: string}) {
-  return (
-    <MarkdownPreview source={source} style={{ padding: 16 }} />
-  )
+  const { theme } = useTheme()
+  if(theme === "light"){
+    return (
+      <MarkdownPreview className='bg-transparent' source={source} style={{ padding: 16 }}   wrapperElement={{  "data-color-mode": "light" }}/>
+    )
+  }else{
+    return (
+      <MarkdownPreview className='bg-transparent' source={source} style={{ padding: 16 }}   wrapperElement={{  "data-color-mode": "dark" }}/>
+    )
+  }
+
 }
