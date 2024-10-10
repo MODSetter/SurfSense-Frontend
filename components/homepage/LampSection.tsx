8fa2decc2de1e0d21caec8f93c078d8ef4d17281
DESKTOP-RTLN3BA\$punk <vermarohanfinal@gmail.com>
2024-10-10 02:41:25 -0700

    ui

--- components/homepage/LampSection.tsx
+++ components/homepage/LampSection.tsx
@@ -1,24 +1,11 @@
 "use client";
 import React from "react";
-import { motion } from "framer-motion";
-import { LampContainer } from "../ui/lamp";
+import { TextHoverEffect } from "@/components/ui/text-hover-effect";
 
 export function LampDemo() {
   return (
-    <LampContainer>
-      <motion.h1
-        initial={{ opacity: 0.5, y: 100 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
-      >
-        Use <span className=" text-purple-400">SurfSense </span>
-        <br /> Never Forget Anything <br />
-      </motion.h1>
-    </LampContainer>
+    <div className="h-[40rem] flex items-center justify-center">
+      <TextHoverEffect text="SURFSENSE" />
+    </div>
   );
 }
